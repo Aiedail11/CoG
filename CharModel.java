e35eb941f2ca014b9de19c3d803f2d0879f8e1ff
Emily Haggard <emilyhaggard02@gmail.com>
2017-04-08 20:28:58 -0400

    created master for worktree setup

---
+++ CharModel.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.util.*;
+
+public class CharModel {
+   //physical attributes
+   int age, height;
+   
+   //stats
+   int endurance, strength, constitution, dexterity, draiocht;
+   
+   String myClass;
+   String name;
+   String race;
+   String background;
+   
+   int num1=0; // just a throwaway int
+   
+  // String[] fields = new String[age, height, ];
+
+  
+
+   
+   public void CharModel() {
+   age = 0;
+   height = 0;
+   endurance = 0;
+   strength = 0;
+   constitution = 0;
+   dexterity = 0;
+   draiocht = 0;
+   
+   }
+   
+ 
+
+}
